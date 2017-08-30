c73aa70774b5825d29528b17c2c2ec5d0e2dbe55
Elazar Broad <elazar.broad@fireeye.com>
2017-08-30 19:02:54 -0400

    Revert "Explicity drop duplicates the right way"
    This reverts commit f3d43ec0e258361986237e709d7b6f5a9796cde6.

--- hxtool_data_models.py
+++ hxtool_data_models.py
@@ -53,8 +53,6 @@ class hxtool_data_models:
 			data_frame = DataFrame(data).astype(str)
 			
 		data_frame.replace('nan', '', inplace = True)
-		# Drop duplicates
-		data_frame.drop_duplicates(subset = group_by, inplace = True)
 		data_frame = data_frame.groupby(by = group_by, as_index = False).apply(lambda _: list(_[index])).reset_index(name = index)
 		data_frame['count'] = data_frame[index].apply(lambda _: len(_))
 		data_frame.sort_values(by = 'count', ascending = False, inplace = True)
