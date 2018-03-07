e18aa97d803dc6f11e18f60dde2249433adcd673
Henrik Olsson <gbghenke@gmail.com>
2018-03-07 16:08:39 +0100

    fixed import issues in task modules

--- hxtool_task_modules/__init__.py
+++ hxtool_task_modules/__init__.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # HXTool packages modules here
-from task_module import *
-from bulk_download_scheduler_task_module import *
-from download_task_module import *
+from .task_module import *
+from .bulk_download_scheduler_task_module import *
+from .download_task_module import *
 
 
 # Customer modules here
