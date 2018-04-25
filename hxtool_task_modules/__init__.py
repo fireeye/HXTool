0603c8da461c4708d579af54044b423563e4eb41
Elazar Broad <elazar.broad@fireeye.com>
2018-04-24 23:16:32 -0400

    Add streaming task module to init

--- hxtool_task_modules/__init__.py
+++ hxtool_task_modules/__init__.py
@@ -10,5 +10,6 @@ from .enterprise_search_task_module import *
 from .stacking_task_module import *
 from .file_listing_task_module import *
 from .file_acquisition_task_module import *
+from .streaming_task_module import *
 
 # Customer modules here
