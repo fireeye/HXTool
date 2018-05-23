38342a8da82aecc407e7cf80a5c9eaafb2768f3f
Elazar Broad <elazar.broad@fireeye.com>
2018-05-23 16:03:23 -0400

    Add bulk acquisition task module

---
+++ hxtool_task_modules/bulk_acquisition_task_module.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hxtool_global
+from .task_module import *
+from hx_lib import *
+
+class bulk_acquisition_task_module(task_module):
+	def __init__(self, parent_task):
+		super(type(self), self).__init__(parent_task)
+
+	@staticmethod
+	def run_args():
+		return [
+			'script',
+			'hostset_id',
+			'skip_base64'
+		]
+		
+	def run(self, script = None, hostset_id = None, skip_base64 = False):
+		ret = False
+		if script:
+			hx_api_object = self.get_task_api_object()	
+			if hx_api_object and hx_api_object.restIsSessionValid():
+				bulk_acquisition_id = None
+				(ret, response_code, response_data) = hx_api_object.restNewBulkAcq(script, hostset_id = hostset_id, skip_base64 = skip_base64)
+				if ret and '_id' in response['data']:
+					bulk_acquisition_id = {'bulk_acquisition_id' : response_data['data']['_id']}
+					self.logger.info("Bulk acquisition ID {} submitted successfully.".format(response_data['data']['_id']))
+				else:
+					self.logger.error("Bulk acquisition submission failed. Response code: {}, response data: {}".format(response_code, response_data))
+			else:
+				self.logger.warn("No task API session for profile: {}".format(self.parent_task.profile_id))
+		else:
+			self.logger.error("'script' is empty!")
+		return(ret, bulk_acquisition_id)
