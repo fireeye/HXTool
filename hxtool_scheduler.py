7c2cf482069001d98e558933bf24d7be50f7165e
Elazar Broad <elazar.broad@fireeye.com>
2018-09-27 10:39:19 -0400

    Make sure next_run is set

--- hxtool_scheduler.py
+++ hxtool_scheduler.py
@@ -247,7 +247,8 @@ class hxtool_scheduler_task:
 			self.start_time = self.next_run
 	
 	def should_run(self):
-		return (self.enabled and  
+		return (self.next_run and
+				self.enabled and  
 				self.state == TASK_STATE_SCHEDULED and
 				(self.parent_complete if self.parent_id and self.wait_for_parent else True) and	
 				datetime.datetime.utcnow() >= self.next_run)
