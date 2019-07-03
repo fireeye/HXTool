fa2cf5039d6ada2a3c1773516e951a9487708f2a
Elazar Broad <elazar.broad@fireeye.com>
2019-07-03 10:34:20 -0400

    Wait until we're all done to commit

--- hxtool_task_modules/x15_postgres_task_module.py
+++ hxtool_task_modules/x15_postgres_task_module.py
@@ -126,8 +126,8 @@ class x15_postgres_task_module(task_module):
 					json.dump(audit_object, buffer)
 					buffer.seek(0)
 					x15_cursor.copy_expert(x15_query, buffer)
-					x15_connection.commit()
 					
+				x15_connection.commit()	
 				x15_connection.close()
 				
 				ret = True								
