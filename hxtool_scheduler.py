6d3eb9b5373c2271e4dff2c2cf3537e1ef6a7331
Elazar Broad <elazar.broad@fireeye.com>
2019-08-15 11:51:49 -0400

    Check ret, as the previous break doesn't break the larger loop

--- hxtool_scheduler.py
+++ hxtool_scheduler.py
@@ -300,20 +300,20 @@ class hxtool_scheduler_task:
 									ret = False
 									self.state = TASK_STATE_FAILED
 									break
-				
-					self.logger.debug("Begin execute {}.{}".format(module.__module__, func))
-					result = getattr(module, func)(*args, **kwargs)
-					self.logger.debug("End execute {}.{}".format(module.__module__, func))
-					if isinstance(result, tuple) and len(result) > 1:
-						ret = result[0]
-						# Store the result - make sure it is of type dict
-						if isinstance(result[1], dict):
-							# Use update so we don't clobber existing values
-							self.stored_result.update(result[1])
-						elif result[1] is not None:
-							self.logger.error("Task module {} returned a value that was not a dictionary or None. Discarding the result.".format(module.__module__))
-					else:
-						ret = result
+					if ret:
+						self.logger.debug("Begin execute {}.{}".format(module.__module__, func))
+						result = getattr(module, func)(*args, **kwargs)
+						self.logger.debug("End execute {}.{}".format(module.__module__, func))
+						if isinstance(result, tuple) and len(result) > 1:
+							ret = result[0]
+							# Store the result - make sure it is of type dict
+							if isinstance(result[1], dict):
+								# Use update so we don't clobber existing values
+								self.stored_result.update(result[1])
+							elif result[1] is not None:
+								self.logger.error("Task module {} returned a value that was not a dictionary or None. Discarding the result.".format(module.__module__))
+						else:
+							ret = result
 					
 					
 					if self._defer_signal:
