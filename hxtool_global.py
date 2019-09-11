8ef21f4dc0f3fee5786bd1ff3ee69d1ed37dcf6a
Elazar Broad <elazar.broad@fireeye.com>
2019-09-11 16:51:08 -0400

    Remove variables from hxtool_global, moved to hxtool_vars

--- hxtool_global.py
+++ hxtool_global.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-
 # The sole purpose of this module is to store global variables and functions
-
-hxtool_schema_version = 40
-root_logger_name = "hxtool"
-data_path = "data"
-log_path = "log"
 
 def initialize():
 	global task_hx_api_sessions
 	task_hx_api_sessions = {}
 	
-	global app_instance_path
-	app_instance_path = None
-	
-def get_submodule_logger_name(name):
-	return "{}.{}".format(root_logger_name, name)	
-	
-def get_logger(submodule_name = None):
-	name = root_logger_name
-	if submodule_name:
-		name = get_submodule_logger_name(submodule_name)
-	return logging.getLogger(name)
+	global hxtool_db
+	global hxtool_config
+	global hxtool_scheduler
+	global hxtool_x15_object
