4587d210be26eb299ef09d04addc280b002fa183
Henrik Olsson <gbghenke@gmail.com>
2019-11-08 13:49:52 +0000

    rewrote apicache, we now support multiple cache types with different interval settings

--- hxtool_apicache.py
+++ hxtool_apicache.py
@@ -9,16 +9,29 @@ from hxtool_scheduler import *
 from hxtool_db import *
 
 class hxtool_api_cache:
-	def __init__(self, hx_api_object, profile_id, fetcher_interval, objects_per_poll, refresh_interval):
+	def __init__(self, hx_api_object, profile_id, intervals, objectTypes):
 		self.logger = hxtool_logging.getLogger(__name__)
 		self.hx_api_object = hx_api_object
 		self.profile_id = profile_id
-		
-		self.fetcher_interval = fetcher_interval
-		self.objects_per_poll = objects_per_poll
-		self.refresh_interval = refresh_interval
 
-		self.cacheObjects = ["host"]
+		# TEMP: drop cache
+		hxtool_global.hxtool_db.cacheDrop(self.profile_id)
+
+		for objectType in objectTypes:
+			if objectType in intervals.keys():
+				try:
+					setattr(self, objectType + "_fetcher_interval", intervals[objectType]["fetcher_interval"])
+					setattr(self, objectType + "_objects_per_poll", intervals[objectType]["objects_per_poll"])
+					setattr(self, objectType + "_refresh_interval", intervals[objectType]["refresh_interval"])
+				except:
+					self.logger.error("Missing interval settings for {}, check configuration to enable cache".format(objectType))
+					exit(2)
+
+				my_fetcher_task = hxtool_scheduler_task("System", "Cache fetcher for " + objectType + " profile: " + str(self.profile_id), immutable=True)
+				my_fetcher_task.set_schedule(seconds=intervals[objectType]["fetcher_interval"])
+				my_fetcher_task.add_step(self, "apicache_fetcher", kwargs={"objectType" : objectType } )
+				hxtool_global.hxtool_scheduler.add(my_fetcher_task)
+				self.logger.info("Apicache {} fetcher started for profile: {}.".format(objectType, self.profile_id))
 
 		# Number of objects that can be fetched per second
 		#self.fetcher_capability = self.objects_per_poll // self.fetcher_interval
@@ -35,97 +48,93 @@ class hxtool_api_cache:
 		#hxtool_global.apicache['updater_capability'] = self.updater_capability
 		#hxtool_global.apicache['updater_capability_attempts'] = self.updater_capability_attempts
 
-		# TEMP: drop cache
-		hxtool_global.hxtool_db.cacheDrop(self.profile_id)
+	def apicache_processor(self, currOffset, objectType, records, myCache, refresh_interval):
 
-		# Schedule fetcher
-		apicache_fetcher_task = hxtool_scheduler_task("System", "Cache fetcher for " + str(self.profile_id), immutable=True)
-		apicache_fetcher_task.set_schedule(seconds=self.fetcher_interval)
-		apicache_fetcher_task.add_step(self, "apicache_fetcher")
-		hxtool_global.hxtool_scheduler.add(apicache_fetcher_task)
-		self.logger.info("Apicache fetcher started for profile: {}.".format(self.profile_id))
+		for record in records:
+			
+			currOffset += 1
 
+			if record['_id'] in myCache.keys():
+				t = datetime.datetime.now() - datetime.datetime.strptime(myCache[record['_id']], "%Y-%m-%d %H:%M:%S")
+				if t.seconds > refresh_interval:
+					hxtool_global.hxtool_db.cacheUpdate(self.profile_id, objectType, record['_id'], record)
+					self.logger.info("{}: {} record updated: {}".format(self.profile_id, objectType, record['_id']))
 
-	def apicache_fetcher(self):
-		#self.logger.info("Apicache fetcher called.")
+					# Special case, also get sysinfo for hosts
+					if objectType == "host":
+						(ret, response_code, response_data) = self.hx_api_object.restGetHostSysinfo(record['_id'])
+						if ret:
+							hxtool_global.hxtool_db.cacheUpdate(self.profile_id, "sysinfo", record['_id'], response_data['data'])
+							self.logger.info("{}: Host sysinfo record updated: {}".format(self.profile_id, record['_id']))
+			else:
+				hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, record)
+				self.logger.info("{}: New {} record added: {}".format(self.profile_id, objectType, record['_id']))
+
+				# Special case, also get sysinfo for hosts
+				if objectType == "host":
+					(ret, response_code, response_data) = self.hx_api_object.restGetHostSysinfo(record['_id'])
+					if ret:
+						hxtool_global.hxtool_db.cacheAddById(self.profile_id, "sysinfo", record['_id'], response_data['data'])
+						self.logger.info("{}: New sysinfo record added: {}".format(self.profile_id, record['_id']))
+
+		return currOffset
+
+	def apicache_fetcher(self, objectType):
+
 		#Temp workaround
 		time.sleep(2)
 
-		for objectType in self.cacheObjects:
-			# Get a list of current cache entries for this object type
-			res = hxtool_global.hxtool_db.cacheList(self.profile_id, objectType)
+		# Get a list of current cache entries for this object type
+		res = hxtool_global.hxtool_db.cacheList(self.profile_id, objectType)
 
-			# Format the local cache results into a dict
-			myCache = {}
-			for cacheEntry in res:
-				myCache[cacheEntry['contentId']] = cacheEntry['update_timestamp']
-			
-			# We always start the query from the top (always check everything)
-			myoffset = 0
-
-			if objectType == "host":
-				while True:
-					(ret, response_code, response_data) = self.hx_api_object.restListHosts(offset=myoffset, sort_term="_id+ascending", limit=self.objects_per_poll)
-					if ret:
-						# Leave loop if no new records are returned
-						if len(response_data['data']['entries']) == 0:
-							break
-
-						# Loop over all the records returned
-						for host in response_data['data']['entries']:
-							myoffset += 1
-							
-							if host['_id'] in myCache.keys():
-								# Record exists, check last update timestamp and update if required
-								t = datetime.datetime.now() - datetime.datetime.strptime(myCache[host['_id']], "%Y-%m-%d %H:%M:%S")
-								if t.seconds > self.refresh_interval:
-									hxtool_global.hxtool_db.cacheUpdate(self.profile_id, "host", host['_id'], host)
-									self.logger.info("{}: Host record updated: {}".format(self.profile_id, host['_id']))
-
-									# For hosts, we also update sysinfo
-									(ret, response_code, response_data) = self.hx_api_object.restGetHostSysinfo(host['_id'])
-									if ret:
-										hxtool_global.hxtool_db.cacheUpdate(self.profile_id, "sysinfo", host['_id'], response_data['data'])
-										self.logger.info("{}: Host sysinfo record updated: {}".format(self.profile_id, host['_id']))
-							else:
-								# No record exists, create a new entry
-								hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, host)
-								self.logger.debug("{}: New host added: {}".format(self.profile_id, host['_id']))
-
-								# For hosts, also get sysinfo
-								(ret, response_code, response_data) = self.hx_api_object.restGetHostSysinfo(host['_id'])
-								if ret:
-									hxtool_global.hxtool_db.cacheAddById(self.profile_id, "sysinfo", host['_id'], response_data['data'])
-									self.logger.debug("{}: New host sysinfo added: {}".format(self.profile_id, host['_id']))
-
+		# Format the local cache results into a dict
+		myCache = {}
+		for cacheEntry in res:
+			myCache[cacheEntry['contentId']] = cacheEntry['update_timestamp']
 		
-#				elif objectType == "alert":
-#					(ret, response_code, response_data) = self.hx_api_object.restGetAlerts(offset=myoffset, limit=self.objects_per_poll)
-#					if ret:
-#						for alert in response_data['data']['entries']:
-#							myoffset += 1
-#							hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, myoffset, alert)
-#							self.logger.debug("{}: New alert added: {}".format(self.profile_id, alert['_id']))
-#				elif objectType == "triage":
-#					(ret, response_code, response_data) = self.hx_api_object.restListTriages(offset=myoffset, limit=self.objects_per_poll)
-#					if ret:
-#						for triage in response_data['data']['entries']:
-#							myoffset += 1
-#							hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, myoffset, triage)
-#							self.logger.debug("{}: New triage added: {}".format(self.profile_id, triage['_id']))
-#				elif objectType == "file":
-#					(ret, response_code, response_data) = self.hx_api_object.restListFileaq(offset=myoffset, limit=self.objects_per_poll)
-#					if ret:
-#						for file in response_data['data']['entries']:
-#							myoffset += 1
-#							hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, myoffset, file)
-#							self.logger.debug("{}: New fileacq added: {}".format(self.profile_id, file['_id']))
-#				elif objectType == "live":
-#					(ret, response_code, response_data) = self.hx_api_object.restListDataAcquisitions(offset=myoffset, limit=self.objects_per_poll)
-#					if ret:
-#						for live in response_data['data']['entries']:
-#							myoffset += 1
-#							hxtool_global.hxtool_db.cacheAdd(self.profile_id, objectType, myoffset, live)
-#							self.logger.debug("{}: New live acquisition added: {}".format(self.profile_id, live['_id']))
+		# We always start the query from the top (always check everything)
+		myoffset = 0
+
+		if objectType == "host":
+			while True:
+				(ret, response_code, response_data) = self.hx_api_object.restListHosts(offset=myoffset, sort_term="_id+ascending", limit=self.host_objects_per_poll)
+				if ret:
+					# Leave loop if no new records are returned
+					if len(response_data['data']['entries']) == 0:
+						break
+					myoffset = self.apicache_processor(myoffset, objectType, response_data['data']['entries'], myCache, self.host_refresh_interval)
+
+		elif objectType == "alert":
+			while True:
+				(ret, response_code, response_data) = self.hx_api_object.restGetAlerts(offset=myoffset, sort_term="_id+ascending", limit=self.alert_objects_per_poll)
+				if ret:
+					if len(response_data['data']['entries']) == 0:
+						break
+					myoffset = self.apicache_processor(myoffset, objectType, response_data['data']['entries'], myCache, self.alert_refresh_interval)
+
+		elif objectType == "triage":
+			while True:
+				(ret, response_code, response_data) = self.hx_api_object.restListTriages(offset=myoffset, sort_term="_id+ascending", limit=self.triage_objects_per_poll)
+				if ret:
+					if len(response_data['data']['entries']) == 0:
+						break
+					myoffset = self.apicache_processor(myoffset, objectType, response_data['data']['entries'], myCache, self.triage_refresh_interval)
+
+		elif objectType == "file":
+			while True:
+				(ret, response_code, response_data) = self.hx_api_object.restListFileaq(offset=myoffset, sort_term="_id+ascending", limit=self.file_objects_per_poll)
+				if ret:
+					if len(response_data['data']['entries']) == 0:
+						break
+					myoffset = self.apicache_processor(myoffset, objectType, response_data['data']['entries'], myCache, self.file_refresh_interval)
+
+		elif objectType == "live":
+			while True:
+				(ret, response_code, response_data) = self.hx_api_object.restListDataAcquisitions(offset=myoffset, sort_term="_id+ascending", limit=self.live_objects_per_poll)
+				if ret:
+					if len(response_data['data']['entries']) == 0:
+						break
+					myoffset = self.apicache_processor(myoffset, objectType, response_data['data']['entries'], myCache, self.live_refresh_interval)
+
 		return True
 
